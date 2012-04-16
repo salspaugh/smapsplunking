01abc9483e038731d49a8b37714081744c24390a
Your Name <saraalspaugh@gmail.com>
2012-04-16 15:55:22 +0000

    Now smapsplunker logs smap data using timed rotating handler.

--- smapsplunker.py
+++ smapsplunker.py
@@ -1,85 +1,58 @@
 #!/usr/bin/env python
 
 from smap.archiver.client import *
-from time import time
 
 import daemon
 import grp
 import lockfile
+import logging
+import logging.config
 import signal
 import sys
 
-class Logger:
+class SmapSplunkingLogger:
 
-    QUERYURL =  "http://ar1.openbms.org:8079"
+    QUERYURL = "http://ar1.openbms.org:8079"
     CORYDATA = "Metadata/Extra/Phase = 'ABC' and \
                 Properties/UnitofMeasure = 'kW' and \
                 Metadata/Location/Building = 'Cory Hall'"
     
     def __init__(self):
-        self.starttime = str(time())
-        self.republisher = RepublishClient(Logger.QUERYURL, self.log, restrict=Logger.CORYDATA)
-        self.logfile = None
-        self.errfile = None
+        logging.config.fileConfig('logging.conf')
+        self.logger = logging.getLogger()
+        self.republisher = RepublishClient(SmapSplunkingLogger.QUERYURL, 
+                             self.log, restrict=SmapSplunkingLogger.CORYDATA)
 
     def log(self, datapoint):        
-        self.logfile.write(''.join([str(datapoint), '\n']))
-        self.logfile.flush()
+        self.logger.critical(''.join([str(datapoint), '\n']))
 
     def run(self):
-        try:
-            self.errfile = open(''.join([self.starttime, ".err"]), 'a')
-        except:
-            # fuck if I know how else to get errors out of this daemon
-            pass
-        try:
-            self.logfile = open(''.join([self.starttime, ".log"]), 'a')
-        except IOError:
-            self.errfile.write("Mysterious IOError.")
-            self.errfile.flush()
         self.republisher.connect()
         reactor.run()
 
     def close(self):
-        self.errfile.close()
-        self.logfile.close()
-        if not self.republisher.close():
-            sys.stderr.write("Couldn't close smap republisher client.\n")
+        self.republisher.close() #TODO: Check if this returns True on exit?
 
 if __name__ == "__main__":
     
-    logger = Logger()
+    sslog = SmapSplunkingLogger()
+
+    fdpreserve = [] # don't close the logging files on daemon startup
+    for handler in sslog.logger.handlers:
+        if hasattr(handler, 'stream') and hasattr(handler.stream, 'fileno'):
+            fdpreserve.append(handler.stream.fileno())
 
     context = daemon.DaemonContext(
         working_directory='/var/smapsplunker',
         umask=0o002,
-        pidfile=lockfile.FileLock('/var/run/smapsplunker.pid')
+        pidfile=lockfile.FileLock('/var/run/smapsplunker.pid'),
+        files_preserve = fdpreserve
         )
 
     context.signal_map = {
-        signal.SIGTERM: logger.close,
+        signal.SIGTERM: sslog.close,
         signal.SIGHUP: 'terminate',
         }
 
     with context:
-        logger.run() 
-        #QUERYURL =  "http://ar1.openbms.org:8079"
-        #CORYDATA = "Metadata/Extra/Phase = 'ABC' and \
-        #            Properties/UnitofMeasure = 'kW' and \
-        #            Metadata/Location/Building = 'Cory Hall'"
-        #
-        #logfile = None
-        #try:
-        #    logfile = open(''.join([str(time()), ".log"]), 'w')
-        #except IOError:
-        #    sys.stderr.write("Unable to open smapsplunker log.\n")
-        #    exit()
-
-        #def log(datapoint):
-        #    logfile.write(''.join([str(datapoint), '\n']))
-        #    logfile.flush()
-
-        #republisher = RepublishClient(QUERYURL, log, restrict=CORYDATA)
-        #republisher.connect()
-
-        #reactor.run()
+        sslog.run() 
